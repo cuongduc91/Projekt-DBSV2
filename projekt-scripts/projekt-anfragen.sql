7afef200e7c4933cf5353c3ce232c87fa3047f98
Cuong <bdcuong91@gmail.com>
2020-07-04 16:49:14 +0200

    projekt_anfragen Datei hinzufuegen

---
+++ projekt-scripts/projekt-anfragen.sql
@@ -0,0 +1,53 @@
+/* 
+1. Geben Sie für einen Auftrag den Namen des Kunden und alle Arbeitsschritte aus
+*/
+
+/**/
+
+/*
+2.Berechnen Sie die angefallenen Arbeitskosten pro Auftrag (ohne die Materialkosten).
+Geben Sie den Namen des Kunden, die Auftragsnummer und die berechneten
+Arbeitskosten aus.
+*/
+
+
+/*
+3.Geben Sie Aufträge aus, deren Angebotspreis kleiner ist als der Peris für die geleistete
+Arbeit und die verwendeten Materialien. (D.h. Aufträge, die ein Verlust waren.)
+*/
+
+
+/*
+4.Geben Sie Entlang der Dimension Auftrag mit den Dimensionselementen
+Auftragskategorie und Auftragsart die Kosten für die Arbeitsleistung aus. D.h. Kosten für Wartung, Kontrolle, Reparatur, Bau jeweils einzeln, dann die Summen je
+Auftragskategorie (also Summe für Service und Produktion) und dann die
+Gesamtsumme über alle Aufträge.
+*/
+
+
+/*
+5.Für die folgenden Anfragen können Sie eine Sicht definieren, um einfachere Select-
+Anweisungen erstellen zu können.
+*/
+
+
+/*
+6.Berechnen Sie für jeden Auftrag seinen Prozentualen Anteil am Gesamtumsatz im Jahr.
+Der Gesamtumsatz soll sich aus den Kosten der Einzelnen Arbeitsschritte und den
+Kosten der verbauten Materialen berechnen.
+*/
+
+/*
+7.Geben Sie die Namen der 3 Kunden aus, die im laufenden Jahr den meisten Umsatz
+erzeugt haben.
+*/
+
+/*
+8.Berechnen Sie den gleitenden Durchschnitt der Arbeitskosten für 5 Tage für die
+Auftragsart „Wartung“.
+*/
+
+
+/*
+Könnte man materialisierte Sichten nutzen, um die Effizienz der Anfragen zu verbessern?
+*/
